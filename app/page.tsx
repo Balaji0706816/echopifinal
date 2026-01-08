d0a342fefa1e82e778b45dff6dfa121656902a18
Balaji Tanniru <balajitanniru@mac.sfw.umb.edu>
2026-01-08 12:34:31 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,17 @@
-"use client"
-import Header from "../components/header/Header";
+"use client";
 
+import Header from "../components/header/Header";
 import PricingCard from "../components/PricingCard";
 import FAQ from "../components/FAQ";
 import FourWays from "../components/FourWays";
 
-
-
 export default function Home() {
   return (
-    <main >
- <Header/>
-
- <FourWays />
- <PricingCard />
- <FAQ />
+    <main>
+      <Header />
+      <FourWays />
+      <PricingCard />
+      <FAQ />
     </main>
   );
 }
